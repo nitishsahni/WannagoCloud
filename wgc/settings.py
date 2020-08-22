0e80114badc85c2c595636d895515ac7088824b0
Nitish Sahni <sahninitish@gmail.com>
2020-08-22 05:24:59 +0000

    Apache changes

--- wgc/settings.py
+++ wgc/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'u!mir4jxvl)f22r&%i^mcwxak99n^phhx(7)v#_(-6(@i99ab3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.248.124.154', ]
 
 # Application definition
 
